d43daa9913f402714fa9988a29b5aec3d2e0dad6
kratos619 <gaurav.palaspagar@hotmail.com>
2018-01-14 23:15:46 +0530

    add main function to index.php

---
+++ index.php
@@ -0,0 +1,500 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<link rel="stylesheet" href="css\bootstrap.css">
+<link rel="stylesheet" href="css\font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="slick-1.8.0/slick-1.8.0/slick/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="slick-1.8.0/slick-1.8.0/slick/slick-theme.css"/>
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Joti+One" rel="stylesheet">
+<link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+<link rel="shortcut icon" href="img/JDlogos.png" />
+<title>The Fashion Show</title>
+</head>
+<body id="home">
+  <nav class="navbar navbar-toggleable-md navbar-light fixed-top py-4">
+      <div class="container">
+        <button class="navbar-toggler navbar-toggler-right"><span class="navbar-toggler-icon" data-toggle="collapse" data-target="#navbarNav"></span></button>
+        <a href="#" class="navbar-brand">
+          <img src="img/JDlogos.png" width="150" height="50" alt=""> <h4 class="d-inline align-middle"><small>Presents </small>The Fashion Show</h4>
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="#home" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="#about" class="nav-link">About</a>
+            </li>
+            <li class="nav-item">
+              <a href="#authors" class="nav-link">Meet The Sopnsers</a>
+            </li>
+            <li class="nav-item">
+              <a href="#contact" class="nav-link">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+<!-- Section Show Case -->
+<section id="showcase" class="py-5">
+  <div class="primary-overlay text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-sm-12 text-center">
+          <h3 style="font-weight: 600;" class="display-4">
+            The <span style="color: #3292a6;"> Fashion</span> Show 
+          </h3>
+          <p class="lead">Dress Like You're Already Famous...</p>
+          
+		  </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- NEWS LETTER 
+
+<section id="news" class="bg-inverse text-white py-5">
+  <div class="container">
+    <form class="row">
+      <div class="col-md-4">
+        <input type="text" name="" class="form-control form-control-lg" placeholder="Name" value="">
+      </div>
+      <div class="col-md-4">
+        <input type="email" name="" class="form-control form-control-lg" placeholder="Email" value="">
+      </div>
+      <div class="col-md-4">
+        <button class="btn btn-btn-primary btn-block btn-lg">
+          <i class="fa fa-envelope-open-o"></i> Subscribe
+        </button>
+      </div>
+    </form>
+  </div>
+</section>
+-->
+
+
+<!--boxes-->
+<secction id="boxes">
+  <div id="about" class="container py-5">
+     <div  data-aos="fade-right" class="info-header mb-5 text-center">
+          <h1 class="text-primary pb-3">
+            Up-comeing Events
+          </h1>
+            <p class="lead pb-3"> Check Out Upcomeing Event Details</p>
+        </div>
+    <div class="row">
+     
+      <div class="col-md-3" data-aos="flip-up" >
+        <div class="card text-center card-outline-primary">
+          <div class="card-block">
+            <h3 class="text-primary">
+              Audition
+            </h3>
+            <p class="text-muted">
+            	18 Dec to 10 feb
+            	Place Nagpur
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3"  data-aos="flip-up">
+        <div class="card text-center card-primary text-white">
+          <div class="card-block">
+            <h3>
+              Entey Fees
+            </h3>
+            <p>
+              Male Model : 1500/- <br>
+              Female Model : 1000/-
+            </p>
+            <br>
+            
+          </div>
+        </div>
+      </div>
+      <div  data-aos="flip-up" class="col-md-3">
+        <div class="card text-center card-outline-primary">
+          <div class="card-block">
+            <h3 class="text-primary">
+              Event Venu
+            </h3>
+            <p class="text-muted">
+              Scientic Hall Nagpur
+              <br>
+              Date: 19 feb 2018
+              <br>
+              Time 7 To 10 PM
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3"  data-aos="flip-up">
+        <div class="card text-center card-primary text-white">
+          <div class="card-block">
+            <h3>
+              For Sponsorship
+            </h3>
+            <p>
+              <i class="fa fa-phone" aria-hidden="true"></i> 9096160005
+              <br>
+             <i class="fa fa-phone" aria-hidden="true"></i> 9766761308
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</secction>
+
+<!-- About section 
+
+<section id="about" class="my-5 py-5 text-center bg-faded">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="info-header mb-5">
+          <h1 class="text-primary pb-3">
+            Up-comeing Events
+          </h1>
+            <p class="lead pb-3"> Check Out The List Of Upcomeing Events</p>
+        </div>
+
+       
+        <div id="accordion" role="tablist">
+          <div class="card">
+            <div class="card-header" role="tab" id="heading1">
+              <h5 class="mb-0">
+                <div href="#collapse1" data-toggle="collapse" data-parent="#accordion">
+                  <i class="fa fa-arrow-down"></i> Auditons
+                </div>
+              </h5>
+            </div>
+            <div id="collapse1" class="collapse show">
+              <div class="card-block">
+                 <p class="lead">Place Nagpur</p>
+                	18 Dec To 15 Feb
+              </div>
+            </div>
+          </div>
+
+
+          <div class="card">
+            <div class="card-header" role="tab" id="heading">
+              <h5 class="mb-0">
+                <div href="#collapse2" data-toggle="collapse" data-parent="#accordion">
+                  <i class="fa fa-arrow-down"></i> Gain The Knwledge
+                </div>
+              </h5>
+            </div>
+            <div id="collapse2" class="collapse">
+              <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt sapiente veniam repellendus, iste, ratione modi perferendis! Ducimus quibusdam aliquid vero.
+              </div>
+            </div>
+          </div>
+
+
+          <div class="card">
+            <div class="card-header" role="tab" id="heading3">
+              <h5 class="mb-0">
+                <div class="collapsed" href="#collapse3" data-toggle="collapse" data-parent="#accordion">
+                  <i class="fa fa-arrow-down"></i> Open Your Mind
+                </div>
+              </h5>
+            </div>
+            <div id="collapse3" class="collapse">
+              <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt sapiente veniam repellendus, iste, ratione modi perferendis! Ducimus quibusdam aliquid vero.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+-->
+
+<!--Hot Pic -->
+<div class="container">
+
+<div data-aos="fade-left" class="info-header mb-5">
+          <h1 class="text-primary pb-3 text-center">
+            Rock The RunWay
+          </h1>
+            <p class="lead pb-3"> 
+            We are now hosting Auditions for the upcomeing Fashion Show Plese Bring Your Heals For Audiotion See The Details For More Information
+            </p>
+        </div>
+<div class="row">
+	<div  data-aos="flip-left" class="col-md-6">
+		<img height="50%" src="img/poster.jpeg" alt="Fashion Week poster" class="img-fluid">
+	</div>
+	<div data-aos="flip-right" class="col-md-6 ">
+	<h2 class="display-3 text-center">
+		The <span style="color: #3292a6;"> Fashion</span> Show 
+	</h2>
+		<p class="lead">
+			“Fashion is what you’re offered four times a year by designers. And style is what you choose.” <br> — Lauren Hutton
+		</p>
+		<br>
+		<br>
+		<div class="text-primary">
+		<h3>Prices</h3>
+		<p class="lead">
+			1st 80,000/- <br>
+			2nd 50,000/- <br>
+			3rd 20,000/-
+		</p>
+		</div>
+		<br>
+		<br>
+		<p style="color:#f02c2c;" class="lead ">
+			Note: If You are 5.3 or Taller And Over The Age of 16 consider Audition for over Show ! No Experience Necessary Free Traning Provided <br>
+			 <i class="fa fa-phone-square" aria-hidden="true"></i> 909616005, 9766761308
+		</p>
+	</div>
+</div>	
+</div>
+<!--Sopnsers-->
+<section data-aos="fade-up"
+     data-aos-duration="3000" id="authors" class="my-5 text-center your-class">
+  <div class="container">
+   
+    <div class="row">
+      <div class="col">
+        <div class="info-header mb-5">
+          <h1 class="text-primary pb-3">
+            Our Sponsers
+          </h1>
+            <p class="lead pb-3">  For your generosity, I thank you..</p>
+        </div>
+
+        <div class="row pt-3 autoplay">
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/JDlogos.png" alt="" class="img-fluid">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/Sponsers_crossworlds.jpeg" alt="" class="img-fluid">
+             
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/JDlogos.png" alt="" class="img-fluid">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/Sponsers_crossworlds.jpeg" alt="" class="img-fluid">
+             
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/JDlogos.png" alt="" class="img-fluid">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/Sponsers_crossworlds.jpeg" alt="" class="img-fluid">
+             
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/JDlogos.png" alt="" class="img-fluid">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12 py-5">
+            <div class="">
+              <img src="img/Sponsers_crossworlds.jpeg" alt="" class="img-fluid">
+             
+            </div>
+          </div>
+</div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--contact -->
+
+<section id="contact" class="bg-faded py-5">
+  <div class="container">
+    <div  data-aos="fade-right" class="info-header mb-5 text-center">
+          <h1 class="text-primary pb-3">
+            Contact Us
+          </h1>
+            <p class="lead pb-3"> Call Or Direct Walk....</p>
+        </div>
+    <div class="row">
+
+
+    <div data-aos="fade-down-right" class="col-lg-9">
+       <div class="card p-4">
+            <div class="card-block">
+              <h3 class="text-center">Please fill out this form to contact us</h3>
+              <hr>
+              <?php
+              
+              if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['phnumber']) && isset($_POST['message'])){
+              // validtion
+              $fname = $_POST['fname'];
+              $lname = $_POST['lname'];
+              $email = $_POST['email'];
+              $phnumber = $_POST['phnumber'];
+              $message = $_POST['message'];
+              
+              if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                  echo "kindly Provide Valid email";
+              }else{
+                  if(!empty($fname) && !empty($email) && !empty($lname) && !empty($phnumber) && !empty($message)){
+                      
+                      $body = $fname . "\n" . $lname . "\n" . $email . "\n" .$phnumber . "\n" . $message;
+                  
+                  $mymailid = "jaykumar.deshmukh@gmail.com";
+                  $subject = "regarding to the fashion show";
+                  $From = "response@jdfashionfair.com";
+                  if(mail($mymailid,$subject,$body,$From)){
+                      echo "successfull";
+                  }else{
+                      echo "something Went Wrong";
+                  }
+                  }else{
+                      echo "submit all the fields";
+                  }
+              }
+              
+                  
+              }//outer isset if end
+              else{
+                  
+              }
+              
+              
+              ?>
+              
+              <form method="post" action="">
+              
+                  <div class="row">
+                  
+                  
+                <div class="col-md-6">
+                  <div class="form-group">
+                      <input name="fname" type="text" class="form-control" placeholder="First Name">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input name="lname" type="text" class="form-control" placeholder="Last Name">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                      <input name="email" type="text" class="form-control" placeholder="Email">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                      <input name="phnumber" type="text" class="form-control" placeholder="Phone Number">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                      <textarea name="message" class="form-control" placeholder="Message"></textarea>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <input type="submit" class="btn btn-outline-danger btn-block" value="Submit">
+                </div>
+              </div>
+              
+              </form>
+              
+            </div>
+
+        </div>
+    </div>
+    <div class="col-lg-3 align-self-center">
+      <img src="img/JDlogos.png" class="img-fluid hidden-md-down" alt="">
+      <br>
+      <br>
+      <h1>OR Call US</h1>
+                           <div class="p-2 item-hl">
+                            <i class="fa fa-phone news-letter-col-2" aria-hidden="true"></i> 9096160005 <br>
+                            <i class="fa fa-phone news-letter-col-2" aria-hidden="true"></i> 9766761308 <br>
+      </div>
+    </div>
+    </div>
+  </div>
+</section>
+
+<footer id="main-footer" class="py-5 bg-primary text-white">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-6">
+        <h3 class="lead">Contact info</h3>
+                    <div class="d-flex flex-column row-h1">
+                        <div class="p-2 item-hl">
+                            <i class="fa fa-phone news-letter-col-2" aria-hidden="true"></i> 9096160005 <br>
+                            <i class="fa fa-phone news-letter-col-2" aria-hidden="true"></i> 9766761308 <br>
+                        </div>
+                        <div class="p-2 item-hl "><i class="fa fa-envelope news-letter-col-2" aria-hidden="true"></i> 
+                        jaykumar.deshmukh@gmail.com
+                        </div>
+                        <div class="p-2 item-hl ">
+                            <i class="fa fa-map-marker news-letter-col-2" aria-hidden="true"></i>  Scientific Hall, Laxminagar,Nagpur
+                        </div>
+                    </div>
+      </div>
+       <div class="col-md-6">
+        <p class="lead">Copyright &copy; 2017 JDeshmukh</p>
+      </div>
+    </div>
+  </div>
+</footer>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/tether.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/navbar-fixed.js"></script>
+ <script type="text/javascript" src="js/jquery.min.js"></script>
+  
+<script type="text/javascript" src="slick-1.8.0/slick-1.8.0/slick/slick.min.js"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.autoplay').slick({
+         slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+      });
+    });
+  </script>
+
+  <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+   <script>
+    AOS.init();
+  </script>
+</body>
+</html>
